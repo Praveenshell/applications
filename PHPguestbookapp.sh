4a95cc4f0123471c0729aef3c170d20796af845b
Praveenshell <praveenshell@gmail.com>
2021-12-16 20:03:33 +0530

    PHPguestbookapp.sh
    commit

---
+++ PHPguestbookapp.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo -e "PHP Guest Book App Deployment \n"
+kubectl apply -f https://k8s.io/examples/application/guestbook/redis-leader-deployment.yaml
+sleep 1
+Kubectl get pods
+sleep 2
+kubectl apply -f https://k8s.io/examples/application/guestbook/redis-leader-service.yaml
+sleep 1
+kubectl apply -f https://k8s.io/examples/application/guestbook/redis-follower-deployment.yaml
+sleep 1
+kubectl get pods
+sleep 2
+kubectl apply -f https://k8s.io/examples/application/guestbook/redis-follower-service.yaml
+sleep 1
+kubectl apply -f https://k8s.io/examples/application/guestbook/frontend-deployment.yaml
+sleep 1
+kubectl get pods -l app=guestbook -l tier=frontend
+sleep 1
+kubectl apply -f https://k8s.io/examples/application/guestbook/frontend-service.yaml
+sleep 2
+kubectl get pods
+sleep 1
+kubectl get services
+sleep 3
+echo -e "Run this command # kubectl edit service frontend \n"
+echo -e "Go to the last before line to change type ClusterIP to LoadBalancer \n"
+echoe -e "You can scale the deployment using cmd # kubectl scale deployment frontend --replicas=5 \n"

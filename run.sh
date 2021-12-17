2cf5e9d3dce71abaa08c00bf1707ea766104e0f3
Praveenshell <praveenshell@gmail.com>
2021-12-17 13:25:12 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,12 +11,12 @@ sleep 1
 kubectl apply -f postgres-pod.yml
 sleep 1
 kubectl apply -f postgres-service.yml
-sleep 1
+sleep 2
+kubectl apply -f worker-app-pod.yml
+sleep 2
 kubectl apply -f result-app-pod.yml
 sleep 1
 kubectl apply -f result-app-service.yml
-sleep 2
-kubectl apply -f worker-app-pod.yml
 echo -e "\n"
 echo -e "success \n"
 echo -e "Voting App deployment completed run # kubectl get svc \n"
